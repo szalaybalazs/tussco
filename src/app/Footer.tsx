5ab7e418c3dedb016c882d8f5490147a6b05add0
Balázs <balazs@szalay.me>
2023-06-17 21:34:24 +0200

    footer hotfix

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -7,7 +7,7 @@ interface iFooterProps {}
 const Footer: FunctionComponent<iFooterProps> = () => {
   return (
     <footer className="flex flex-col px-4 sm:flex-row pt-4 max-w-4xl mb-12 pb-12 gap-8 sm:gap-0 mx-auto w-full border-t border-gray-300 h-[80px]">
-      <div className="flex flex-col flex-1 gap-1">
+      <div className="flex flex-col flex-1 gap-1 mb-24">
         <h2 className="text-xl font-black text-indigo-500">Tuss Co.</h2>
         <p className="text-md font-medium text-gray-500">Budapest, Hungary</p>
         <a
@@ -17,13 +17,13 @@ const Footer: FunctionComponent<iFooterProps> = () => {
           hello@laszlotuss.com
         </a>
       </div>
-      <div className="flex flex-col flex-1 gap-1">
+      <div className="flex flex-col flex-1 gap-1 mb-24">
         <span className="text-sm mb-2 font-medium text-gray-500">Apps</span>
         {apps.map((app) => {
           return (
             <Link
               className="text-md font-medium hover:underline underline-offset-2"
-              href={`/app/${app.id}`}
+              href={`/${app.id}`}
               key={app.id}
             >
               {app.name}
@@ -31,7 +31,7 @@ const Footer: FunctionComponent<iFooterProps> = () => {
           );
         })}
       </div>
-      <div className="flex flex-col flex-1 gap-1">
+      <div className="flex flex-col flex-1 gap-1 mb-24">
         <span className="text-sm mb-2 font-medium text-gray-500">Legal</span>
         <Link
           className="text-md font-medium hover:underline underline-offset-2"
